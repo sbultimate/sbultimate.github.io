d87463116de2c1e0d7f003aca4f21f428b2c54dd
sbultimate <panicultimate@gmail.com>
2018-08-02 10:14:51 -0400

    Initial changes to nav bar with dropdown. Mobile handling not  yet included.

--- myscripts.js
+++ myscripts.js
@@ -80,20 +80,40 @@ function setDropDown() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onmouseover = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+// window.onmouseover = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+if (!event.target.matches('.dropbtn')) {
+
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
     }
   }
 }
-
+}
 // showSlides();
 // showSlides2();
 // showSlides3();
